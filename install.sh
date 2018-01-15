e01609c0e285b10a780d82f8a3daf65914100a75
micchy326 <micchy326@gmail.com>
2018-01-15 22:45:46 +0900

    add vscode install script

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+mkdir -p ~/.config/Code/User/
+ln -s ~/dotfiles/settings.json ~/.config/Code/User/settings.json
+ln -s ~/dotfiles/keybindings.json ~/.config/Code/User/keybindings.json
+
